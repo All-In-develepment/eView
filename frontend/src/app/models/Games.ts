0e125b7725dc6050324ada5804e0c2717e66c9ff
ronaldocestrela <ronaldo@grupoall.in>
2024-02-27 19:20:28 -0300

    front funcioana

---
+++ frontend/src/app/models/Games.ts
@@ -0,0 +1,28 @@
+export type GamesProps = Game[]
+
+export interface Game {
+  id: number
+  date: number
+  stars: number
+  team1?: Team1
+  team2?: Team2
+  format: string
+  event?: Event
+  live: boolean
+  title?: string
+}
+
+export interface Team1 {
+  name: string
+  id: number
+}
+
+export interface Team2 {
+  name: string
+  id: number
+}
+
+export interface Event {
+  id: number
+  name: string
+}
