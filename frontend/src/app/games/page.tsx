c113eafd5114df044ce62a9f5a86906acbb5daf9
Gabriel Silva <biel.silva2802@gmail.com>
2024-02-29 18:18:04 -0300

    fix: correção do tratamento da data

--- frontend/src/app/games/page.tsx
+++ frontend/src/app/games/page.tsx
@@ -1,40 +1,39 @@
 "use client";
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { GamesProps } from '../models/Games'
-import { fromUnixTime, set } from 'date-fns';
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { GamesProps } from "../models/Games";
+import { fromUnixTime, set } from "date-fns";
 
 export default function GetGames() {
-  
-  const [ listGames, setListGames ] = useState<GamesProps[]>([]);
-  
+  const [listGames, setListGames] = useState<GamesProps>([]);
+
   const getGames = async () => {
-    const response = (await axios.get<GamesProps>('http://localhost:5000/games')).data;
-    console.log(response)
-    response.map((game) => {
-      game.date = fromUnixTime(game.date / 1000);
-      // setListGames([...listGames, [game]]);
+    const { data } = await axios.get<GamesProps>("http://localhost:5000/games");
+
+    const newData = data.map((game) => {
+      return {
+        ...game,
+        date: fromUnixTime(game.date / 1000),
+      };
     });
-  }
-  
+    setListGames(newData);
+  };
+
   useEffect(() => {
     getGames();
   }, []);
 
-  console.log(typeof listGames)
-
   try {
-    
     // console.log(response.data)
     return (
       <>
         <h1>Games</h1>
-        <div className='grid grid-cols-3 gap-3'>
+        <div className="grid grid-cols-3 gap-3">
           <div>Primeira coluna</div>
-          <div className='grid grid-cols-subgrid gap-3 col-span-2'>
-            
-          </div>
+          {listGames.map((game) => (
+            <p key={game.id}>{game.id}</p>
+          ))}
+          <div className="grid grid-cols-subgrid gap-3 col-span-2"></div>
           <div>Terceira coluna</div>
         </div>
       </>
@@ -42,4 +41,4 @@ export default function GetGames() {
   } catch (error) {
     console.error(error);
   }
-} 
+}
